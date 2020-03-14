c6287c54c1d8d618a09f760a452c890ee0ad37f4
xuliang <18656756681@163.com>
2020-03-14 10:39:03 +0800

    add

---
+++ src/test/java/com/liangxu/dailypractice/DailypracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liangxu.dailypractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailypracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
